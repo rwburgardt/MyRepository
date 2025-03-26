fa7da1747d08fbf18092fb940c12554185922a77
rwburgardt <mintgreenmustang@gmail.com>
2025-03-26 13:43:23 -0500

    Add files via upload

--- recaps.js
+++ recaps.js
@@ -3,50 +3,50 @@ const schedule = [
   id: 'mar27',
   date: 'March 27',
   opp: 'vs Boston',
-  score: 'W 5-4',
+  score: 'W - L',
   recap: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde? Aliquid neque vel cupiditate vero vitae necessitatibus molestias exercitationem, veniam ipsum commodi nam eligendi? Possimus necessitatibus aperiam rerum cumque adipisci in harum incidunt sint? Repellat illo quam facere! Molestiae, facilis. Earum repellat est autem, sequi facere vitae odio placeat nulla magni porro corrupti dolore eligendi adipisci quos quibusdam. Repellat totam voluptate, ipsam recusandae tempora eos. Architecto corporis repellendus enim aliquam? Doloribus illo reiciendis beatae illum ducimus repudiandae rem dolores? Possimus, commodi voluptatum cum nihil sit illo enim necessitatibus magnam. Quisquam sit necessitatibus exercitationem commodi aliquid nam vel perferendis provident distinctio.'
   },
   {
   id: 'mar28',
   date: 'March 28',
   opp: 'vs Boston',
-  score: 'W 2-1',
-  recap: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde? Aliquid neque vel cupiditate vero vitae necessitatibus molestias exercitationem, veniam ipsum commodi nam eligendi? Possimus necessitatibus aperiam rerum cumque adipisci in harum incidunt sint? Repellat illo quam facere! Molestiae, facilis. Earum repellat est autem, sequi facere vitae odio placeat nulla magni porro corrupti dolore eligendi adipisci quos quibusdam. Repellat totam voluptate, ipsam recusandae tempora eos. Architecto corporis repellendus enim aliquam? Doloribus illo reiciendis beatae illum ducimus repudiandae rem dolores? Possimus, commodi voluptatum cum nihil sit illo enim necessitatibus magnam. Quisquam sit necessitatibus exercitationem commodi aliquid nam vel perferendis provident distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde?'
+  score: '',
+  recap: ''
   },
   {
   id: 'mar29',
   date: 'March 29',
   opp: 'vs Boston',
-  score: 'W 7-0',
-  recap: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde? Aliquid neque vel cupiditate vero vitae necessitatibus molestias exercitationem, veniam ipsum commodi nam eligendi? Possimus necessitatibus aperiam rerum cumque adipisci in harum incidunt sint? Repellat illo quam facere! Molestiae, facilis. Earum repellat est autem, sequi facere vitae odio placeat nulla magni porro corrupti dolore eligendi adipisci quos quibusdam. Repellat totam voluptate, ipsam recusandae tempora eos. Architecto corporis repellendus enim aliquam? Doloribus illo reiciendis beatae illum ducimus repudiandae rem dolores? Possimus, commodi voluptatum cum nihil sit illo enim necessitatibus magnam. Quisquam sit necessitatibus exercitationem commodi aliquid nam vel perferendis provident distinctio.'
+  score: '',
+  recap: ''
   },
   {
   id: 'mar30',
   date: 'March 30',
   opp: 'vs Boston',
-  score: 'W 5-4',
-  recap: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde? Aliquid neque vel cupiditate vero vitae necessitatibus molestias exercitationem, veniam ipsum commodi nam eligendi? Possimus necessitatibus aperiam rerum cumque adipisci in harum incidunt sint? Repellat illo quam facere! Molestiae, facilis. Earum repellat est autem, sequi facere vitae odio placeat nulla magni porro corrupti dolore eligendi adipisci quos quibusdam. Repellat totam voluptate, ipsam recusandae tempora eos. Architecto corporis repellendus enim aliquam? Doloribus illo reiciendis beatae illum ducimus repudiandae rem dolores? Possimus, commodi voluptatum cum nihil sit illo enim necessitatibus magnam. Quisquam sit necessitatibus exercitationem commodi aliquid nam vel perferendis provident distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde?'
+  score: '',
+  recap: ''
   },
   {
   id: 'mar31',
   date: 'March 31',
   opp: 'at Cincinnati',
-  score: 'L 3-2',
-  recap: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde? Aliquid neque vel cupiditate vero vitae necessitatibus molestias exercitationem, veniam ipsum commodi nam eligendi? Possimus necessitatibus aperiam rerum cumque adipisci in harum incidunt sint? Repellat illo quam facere! Molestiae, facilis. Earum repellat est autem, sequi facere vitae odio placeat nulla magni porro corrupti dolore eligendi adipisci quos quibusdam. Repellat totam voluptate, ipsam recusandae tempora eos. Architecto corporis repellendus enim aliquam? Doloribus illo reiciendis beatae illum ducimus repudiandae rem dolores? Possimus, commodi voluptatum cum nihil sit illo enim necessitatibus magnam. Quisquam sit necessitatibus exercitationem commodi aliquid nam vel perferendis provident distinctio.'
+  score: '',
+  recap: ''
   },
   {
     id: 'apr1',
     date: 'April 1',
     opp: 'at Cincinnati',
-    score: 'L 5-3',
-    recap: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde? Aliquid neque vel cupiditate vero vitae necessitatibus molestias exercitationem, veniam ipsum commodi nam eligendi? Possimus necessitatibus aperiam rerum cumque adipisci in harum incidunt sint? Repellat illo quam facere! Molestiae, facilis. Earum repellat est autem, sequi facere vitae odio placeat nulla magni porro corrupti dolore eligendi adipisci quos quibusdam. Repellat totam voluptate, ipsam recusandae tempora eos. Architecto corporis repellendus enim aliquam? Doloribus illo reiciendis beatae illum ducimus repudiandae rem dolores? Possimus, commodi voluptatum cum nihil sit illo enim necessitatibus magnam. Quisquam sit necessitatibus exercitationem commodi aliquid nam vel perferendis provident distinctio.'
+    score: '',
+    recap: ''
     },
     {
     id: 'apr2',
     date: 'April 2',
     opp: 'at Cincinnati',
-    score: 'W 1-0',
-    recap: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit odit ipsa, a harum natus officia earum eius, quibusdam facere expedita aspernatur. Nulla non asperiores optio consectetur minima corporis unde? Aliquid neque vel cupiditate vero vitae necessitatibus molestias exercitationem, veniam ipsum commodi nam eligendi? Possimus necessitatibus aperiam rerum cumque adipisci in harum incidunt sint? Repellat illo quam facere! Molestiae, facilis. Earum repellat est autem, sequi facere vitae odio placeat nulla magni porro corrupti dolore eligendi adipisci quos quibusdam. Repellat totam voluptate, ipsam recusandae tempora eos. Architecto corporis repellendus enim aliquam? Doloribus illo reiciendis beatae illum ducimus repudiandae rem dolores? Possimus, commodi voluptatum cum nihil sit illo enim necessitatibus magnam. Quisquam sit necessitatibus exercitationem commodi aliquid nam vel perferendis provident distinctio.'
+    score: '',
+    recap: ''
     },
     {
     id: 'apr4',
@@ -61,7 +61,28 @@ const schedule = [
     opp: 'Tampa Bay',
     score: '',
     recap: ''
-    }
+    },
+    {
+    id: 'apr6',
+    date: 'April 6',
+    opp: 'Tampa Bay',
+    score: '',
+    recap: ''
+    },
+    {
+    id: 'apr7',
+    date: 'April 7',
+    opp: 'at Chicago Cubs',
+    score: '',
+    recap: ''
+    },
+    {
+      id: 'apr8',
+      date: 'April 8',
+      opp: 'at Chicago Cubs',
+      score: '',
+      recap: ''
+    }, 
 ]
 
 let html = '';
